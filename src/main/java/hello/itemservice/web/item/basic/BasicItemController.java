a169b51c3cff4b1557e7bec195701ac07a919791
irerin07 <counterchord89@gmail.com>
2021-05-30 14:00:13 +0900

    1

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.PostConstruct;
 import java.util.List;
+
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -28,10 +30,70 @@ public class BasicItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model){
+    public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
 
-        model.addAttribute("item" ,item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam int quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveV2(@ModelAttribute("item") Item item,
+                       Model model) {
+        //ModelAttribute는 지정된 모델 객체를 만들어 주고 자동으로 model에 지정된 이름("item")으로 넣어준다.
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveV3(@ModelAttribute Item item) {
+        //ModelAttribute는 지정된 모델 객체를 만들어 주고 자동으로 model에 지정된 이름("item")으로 넣어준다.
+        //이름을 따로 지정하지 않으면 efault로 지정된 모델 객체의 이름으로 "item" model에 넣어준다.
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String saveV4(Item item) {
+        //ModelAttribute는 지정된 모델 객체를 만들어 주고 자동으로 model에 지정된 이름("item")으로 넣어준다.
+        //이름을 따로 지정하지 않으면 efault로 지정된 모델 객체의 이름으로 "item" model에 넣어준다.
+        //ModelAttribute도 생략 가능하다.
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
 
         return "basic/item";
     }
